e47c1118c596117a35fb1b1dbaaf5b475b7a5d3e
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-02-28 10:11:10 +0600

    Initial Commit

---
+++ All type of view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All type of view
+//
+//  Created by Shishir_Mac on 28/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
